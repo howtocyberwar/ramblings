b58d3ebe814a9a268aefb13f7d658461fd3d52bd
howtocyberwar <133647473+howtocyberwar@users.noreply.github.com>
2023-05-24 14:28:31 +0200

    Update jsontocsv.py

--- jsontocsv.py
+++ jsontocsv.py
@@ -2,39 +2,40 @@ import json
 import csv
 import sys
 
-def load_domains(file_path):
+def convert_json_to_csv(json_file, field, domains_dict):
+    output_file = f'{field}.csv'
+    field_to_domains_file = f'{field}-to-domains.csv'
+
+    with open(json_file, 'r') as f_json, open(output_file, 'w', newline='') as f_output, open(field_to_domains_file, 'w', newline='') as f_field_domains:
+        json_data = (json.loads(line) for line in f_json)
+        writer = csv.writer(f_output)
+        field_domains_writer = csv.writer(f_field_domains)
+
+        writer.writerow(['id', field])
+        field_domains_writer.writerow(['field_id', 'domain_id'])
+
+        next_id = 1
+
+        for obj in json_data:
+            domain = obj['input']
+            field_value = obj.get(field)
+
+            if field_value:
+                field_id = next_id
+                writer.writerow([field_id, field_value])
+                next_id += 1
+
+                domain_id = domains_dict.get(domain)
+                if domain_id:
+                    field_domains_writer.writerow([field_id, domain_id])
+
+def load_domains(domains_file):
     domains_dict = {}
-    with open(file_path, 'r') as f:
-        reader = csv.reader(f)
+    with open(domains_file, 'r') as f_domains:
+        reader = csv.reader(f_domains)
         for row in reader:
             domains_dict[row[1]] = int(row[0])
     return domains_dict
-
-def convert_json_to_csv(json_file, field, domains_dict):
-    field_dict = {}
-    next_id = 1
-    field_csv_file = f'{field}.csv'
-    field_domain_csv_file = f'{field}-to-domains.csv'
-    
-    with open(json_file, 'r') as f_json, \
-         open(field_csv_file, 'w', newline='') as f_csv, \
-         open(field_domain_csv_file, 'w', newline='') as fd_csv:
-        json_data = json.load(f_json)
-        field_writer = csv.writer(f_csv)
-        field_domain_writer = csv.writer(fd_csv)
-        
-        field_writer.writerow(['id', field])
-        field_domain_writer.writerow(['field_id', 'domain_id'])
-        
-        for obj in json_data:
-            domain = obj.get('input')
-            if domain in domains_dict:
-                field_value = obj.get(field)
-                if field_value and field_value not in field_dict:
-                    field_dict[field_value] = next_id
-                    field_writer.writerow([next_id, field_value])
-                    field_domain_writer.writerow([next_id, domains_dict[domain]])
-                    next_id += 1
 
 def main(json_file, field, domains_file):
     domains_dict = load_domains(domains_file)
@@ -42,10 +43,11 @@ def main(json_file, field, domains_file):
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
-        print("Usage: python script.py data.json field domains.csv")
+        print("Usage: python jsontocsv.py data.json field domains.csv")
         sys.exit(1)
 
     json_file = sys.argv[1]
     field = sys.argv[2]
     domains_file = sys.argv[3]
+
     main(json_file, field, domains_file)
