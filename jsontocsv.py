e140e1a05ffce8b39f4308a141bdcd8697572598
howtocyberwar <133647473+howtocyberwar@users.noreply.github.com>
2023-05-24 14:59:56 +0200

    Update jsontocsv.py

--- jsontocsv.py
+++ jsontocsv.py
@@ -2,49 +2,62 @@ import csv
 import json
 import sys
 
-def convert_json_to_csv(json_file, field, domains_file):
-    # Load the domains data from the CSV file
+json_file = sys.argv[1]
+field = sys.argv[2]
+domains_file = sys.argv[3]
+
+def load_domains(file):
     domains_dict = {}
-    with open(domains_file, 'r') as f_domains:
-        csv_reader = csv.reader(f_domains)
-        next(csv_reader)  # Skip the header
-        for row in csv_reader:
+    with open(file, 'r') as f:
+        reader = csv.reader(f)
+        next(reader)  # Skip the header row
+        for row in reader:
             domain_id, domain = row
             domains_dict[domain] = domain_id
+    return domains_dict
 
-    # Open the output CSV files for writing
-    output_csv_file = f'{field}.csv'
-    field_to_domains_csv_file = f'{field}-to-domains.csv'
-    with open(output_csv_file, 'w', newline='') as f_output, open(field_to_domains_csv_file, 'w', newline='') as f_field_to_domains:
-        output_csv_writer = csv.writer(f_output)
-        field_to_domains_csv_writer = csv.writer(f_field_to_domains)
+def convert_json_to_csv(file, field, domains_dict):
+    csv_file = f"{field}.csv"
+    csv_to_domains_file = f"{field}-to-domains.csv"
+    field_dict = {}
+    domain_to_id = {}
 
-        # Write the headers to the output CSV files
-        output_csv_writer.writerow(['id', field])
-        field_to_domains_csv_writer.writerow(['field_id', 'domain_id'])
+    with open(file, 'r') as f_json, open(csv_file, 'w', newline='') as f_csv, open(csv_to_domains_file, 'w', newline='') as f_domains_csv:
+        writer_csv = csv.writer(f_csv)
+        writer_domains_csv = csv.writer(f_domains_csv)
 
-        # Read the JSON data from the input file and convert it to CSV
-        with open(json_file, 'r') as f_json:
-            for line in f_json:
+        # Write the header row for CSV files
+        writer_csv.writerow(['id', field])
+        writer_domains_csv.writerow(['field_id', 'domain_id'])
+
+        for line in f_json:
+            try:
                 data = json.loads(line)
-                input_value = data['input']
-                field_value = data[field]
-                domain_id = domains_dict.get(input_value)
+                input_val = data.get('input')
+                field_val = data.get(field)
 
-                # Write the data to the output CSV files
-                output_csv_writer.writerow([domain_id, field_value])
-                field_to_domains_csv_writer.writerow([domain_id, domain_id])
+                if input_val and field_val:
+                    if field_val not in field_dict:
+                        field_dict[field_val] = len(field_dict) + 1
 
-def main(json_file, field, domains_file):
-    print(f"Processing {json_file}")
-    print(f"Extracting field: {field}")
+                    field_id = field_dict[field_val]
 
-    convert_json_to_csv(json_file, field, domains_file)
+                    if input_val in domains_dict:
+                        domain = domains_dict[input_val]
 
-    print("Conversion complete")
+                        if domain not in domain_to_id:
+                            domain_to_id[domain] = len(domain_to_id) + 1
 
-if __name__ == "__main__":
-    json_file = sys.argv[1]
-    field = sys.argv[2]
-    domains_file = sys.argv[3]
-    main(json_file, field, domains_file)
+                        domain_id = domain_to_id[domain]
+
+                        writer_csv.writerow([field_id, field_val])
+                        writer_domains_csv.writerow([field_id, domain_id])
+
+            except json.JSONDecodeError:
+                print(f"Invalid JSON object: {line}")
+
+    print(f"Conversion to CSV complete for field: {field}")
+
+domains_dict = load_domains(domains_file)
+
+convert_json_to_csv(json_file, field, domains_dict)
